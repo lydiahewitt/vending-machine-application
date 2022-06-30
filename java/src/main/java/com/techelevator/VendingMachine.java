07ac13b436808a0b85697c4083d629e8b961e5c9
Gregor Dzierzon <gregor.dzierzon@techelevator.com>
2022-06-29 22:49:19 -0600

    initial code push

---
+++ java/src/main/java/com/techelevator/VendingMachine.java
@@ -0,0 +1,11 @@
+package com.techelevator;
+
+import com.techelevator.application.VendingMachineApplication;
+
+public class VendingMachine {
+
+	public static void main(String[] args) {
+		VendingMachineApplication application = new VendingMachineApplication();
+		application.run();
+	}
+}
---
+++ java/src/main/java/com/techelevator/application/VendingMachineApplication.java
@@ -0,0 +1,22 @@
+package com.techelevator.application;
+
+public class VendingMachineApplication {
+
+    public void run() {
+
+        while(true) {
+            // todo: display home screen
+            // todo: and get user choice
+            String userChoice = "display";
+
+            if(userChoice.equalsIgnoreCase("display")) {
+                // todo: logic to display all vending machine items
+            } else if(userChoice.equalsIgnoreCase("purchase")) {
+                // todo: logic to display purchase options
+            } else if(userChoice.equalsIgnoreCase("exit")) {
+                // break out of the loop and end the application
+                break;
+            }
+        }
+    }
+}
